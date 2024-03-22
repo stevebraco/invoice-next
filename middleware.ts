8edd8289b865e4dc222f0058fdc52f4db2d3f4cf
steve <steve.braco@gmail.com>
2024-03-23 00:37:16 +0100

    bug type

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: [],
-  ignoredRoutes: [],
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
